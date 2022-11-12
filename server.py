d506feeee1e4b75c6cf656c9062158bacf3ccd87
Em Welter <90579260+Undertovv@users.noreply.github.com>
2022-11-12 21:31:04 +0000

    Boilerplate for Flask Webserver

---
+++ server.py
@@ -0,0 +1,21 @@
+#/* --- Imports --- */#
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+#/* --- Main --- */#
+@app.route('/templates')
+@app.route('/index.html')
+def index(name=None): #Index page
+    return render_template('index.html', name=name)
+
+@app.route('/templates')
+@app.route('/about.html')
+def about(name=None): #About page
+    return render_template('about.html', name=name)
+
+
+#/* start the server */#
+if __name__ == '__main__':
+    app.run()
