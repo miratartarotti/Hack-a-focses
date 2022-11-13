aa629eb0b94dbc8b2ce650def13341aba88f89fb
Samo Novák <samuel.novak@protonmail.com>
2022-11-13 04:44:11 +0000

    Adding comments: server side

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 #/* --- Imports --- */#
 import os
-import test_db
+import datetime
 
-from flask import Flask, Markup
+from flask import Flask, Markup, request, abort
 from flask import render_template
+
+import test_db
 
 
 app = Flask(__name__)
@@ -30,9 +32,31 @@ def admin():
 def about(name=None): #About page
     return render_template('test_db.html', name=name)
 
-@app.route("/api/notes/<lecture_notes>/comments/s<sentence>")
-def load_comments(lecture_notes, sentence):
-    return test_db.get_comments(lecture_notes, sentence, html = True)
+@app.route("/api/notes/<lecture_id>/comments/s<sentence>")
+def load_comments(lecture_id, sentence):
+    return test_db.get_comments(lecture_id, sentence, html = True)
+
+@app.route("/api/notes/<lecture_id>/comments/s<sentence>/add", methods=["POST"])
+def add_comment(lecture_id, sentence):
+    author = request.form["author"]
+    comment = request.form["comment"]
+    date = datetime.datetime.now()
+
+    print(f"Attempting to add comment by {author} on {date}:\n\t\"{comment}\"")
+
+    conn = test_db.create_connection(test_db.DATABASE)
+    if conn is None:
+        return abort(500)
+
+    cur = conn.cursor()
+    try:
+        test_db.insert_comment(cur, lecture_id, sentence, comment, author, date)
+    except Exception as e:
+        print(e)
+        return abort(500)
+    finally:
+        conn.commit()
+    return "OK"
 
 @app.route("/l<lecture_id>")
 def load_lecture(lecture_id):
